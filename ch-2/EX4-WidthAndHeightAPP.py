1bc5c6390b27dc1743d70a8b0fe8cf0b49573d79
Mahmoud Magdy <103686429+Mahmoud-Magdy17@users.noreply.github.com>
2023-07-26 15:06:30 +0300

    Width And Height APP

---
+++ ch-2/EX4-WidthAndHeightAPP.py
@@ -0,0 +1,10 @@
+width = 17
+height = 12.0
+First = width//2
+print(First, type(First))
+Second = width/2.0
+print(Second, type(First))
+Third = height/3
+print(Third, type(First))
+Fourth = 1 + 2 * 5
+print(Fourth, type(First))
