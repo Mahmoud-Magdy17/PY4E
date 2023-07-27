460a71959f2424526b35a35a7c494862b8d5ba6b
Mahmoud Magdy <103686429+Mahmoud-Magdy17@users.noreply.github.com>
2023-07-27 11:52:40 +0300

    New Feature to Employees App

--- ch-2/Ex2.3_Payment_Of_Employees_App.py
+++ ch-2/Ex2.3_Payment_Of_Employees_App.py
@@ -1,5 +1,21 @@
-# Find Payment of employees
-Hours = input("Enter Hours:")
-RatePerHours = input("Enter The Rate per Hour: ")
-Pay = float(Hours) * float(RatePerHours)
-print(Pay)
+# Find Payment of employees & if Worked +40 Hours give 1.5 Rate
+SHours = input("Enter Hours:")
+try:
+    FHours = float(SHours)
+except:
+    print("Hours You Entered Is Invalid -Might be String not number-")
+    FHours = -1
+SRatePerHours = input("Enter The Rate per Hour: ")
+try:
+    FRatePerHours = float(SRatePerHours)
+except:
+    print("Rate Per Hours You Entered Is Invalid -Might be String not number-")
+    FRatePerHours = -1
+if FHours > 40:
+    Pay = (40*FRatePerHours)+((FHours-40)*(FRatePerHours*1.5))
+else:
+    Pay = FRatePerHours * FHours
+if FRatePerHours == -1 or FHours == -1:
+    print("Finished with errors")
+else:
+    print("Pay: ", Pay)
